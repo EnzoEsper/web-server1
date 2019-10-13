9da0dcbca368834f4cf4242aeb97cb75f970a325
enzoesper <enzoesper@hotmail.com>
2019-10-13 19:48:22 -0300

    wiring up the user interface with the backend

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,27 @@
 console.log("Client side javascript file loaded!");
 
-fetch("http://localhost:5000/weather?address=resistencia").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch(`http://localhost:5000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
   });
 });
