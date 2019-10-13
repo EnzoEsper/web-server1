91fd52bd327e7a3808576a452d8a1cbc68a0c392
enzoesper <enzoesper@hotmail.com>
2019-10-12 21:49:53 -0300

    Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,19 +3,31 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello express!");
+  res.send("<h1>Hello Express!</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help page!");
+  res.send([
+    {
+      name: "dada",
+      age: 69
+    },
+    {
+      name: "life",
+      age: 33
+    }
+  ]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page!");
+  res.send("<h1>About</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather page!");
+  res.send({
+    forecast: "Its snowing",
+    location: "Philadelphia"
+  });
 });
 
 app.listen(5000, () => {
