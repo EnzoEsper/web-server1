e76d916ead17c4daff8c594b8fbd3b6e8ec65787
enzoesper <enzoesper@hotmail.com>
2019-10-12 23:22:56 -0300

    using handlebars template engine to render dynamic content

--- src/app.js
+++ src/app.js
@@ -4,8 +4,33 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+// telling express which template engine we install (setting up handlebars)
+app.set("view engine", "hbs");
 // configurando express para que sirva el directorio public
 app.use(express.static(publicDirectoryPath));
+
+// render the handlebars index file and passing the objet as an argument for it acces on the file
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Handlebars title",
+    name: "dada"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Nyan"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    text:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nobis voluptate libero molestias consequatur perspiciatis numquam tempora, veniam dolor. Tempora, nisi sapiente? Eaque enim culpa ex reiciendis obcaecati odit porro?"
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
