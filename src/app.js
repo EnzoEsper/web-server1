e1adb7b7319043d094bcde41be0482e850aa1134
enzoesper <enzoesper@hotmail.com>
2019-10-13 02:41:55 -0300

    getting information from the requests with query string

--- src/app.js
+++ src/app.js
@@ -45,9 +45,30 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide a address!"
+    });
+  }
+
   res.send({
     forecast: "Its snowing",
-    location: "Philadelphia"
+    location: "Philadelphia",
+    address: req.query.address
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term!"
+    });
+  }
+
+  console.log(req.query);
+
+  res.send({
+    products: []
   });
 });
 
