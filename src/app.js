d0853ff31515670648f632baa5fc9c63a2ba8c54
enzoesper <enzoesper@hotmail.com>
2019-10-13 00:22:39 -0300

    advanced templating using partials of handlebars to reuse certain parts of the page

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup Handlebars and views location
 // telling express which template engine we install (setting up handlebars)
 app.set("view engine", "hbs");
 // changing the default location of the views
 app.set("views", viewsPath);
-
+// configure the partials path directory
+hbs.registerPartials(partialsPath);
 // Setup static directoy to serve
 // configurando express para que sirva el directorio public
 app.use(express.static(publicDirectoryPath));
@@ -34,9 +37,10 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "Help page",
     text:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nobis voluptate libero molestias consequatur perspiciatis numquam tempora, veniam dolor. Tempora, nisi sapiente? Eaque enim culpa ex reiciendis obcaecati odit porro?"
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nobis voluptate libero molestias consequatur perspiciatis numquam tempora, veniam dolor. Tempora, nisi sapiente? Eaque enim culpa ex reiciendis obcaecati odit porro?",
+    title: "Help page",
+    name: "dada life"
   });
 });
 
